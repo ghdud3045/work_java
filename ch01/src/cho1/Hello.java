3704885be3b3a4c414637151987a3c312e3fd615
KOITT <KOITT@KOITT-PC>
2018-05-21 21:31:27 +0900

    보이루 보겸쓰

--- ch01/src/cho1/Hello.java
+++ ch01/src/cho1/Hello.java
@@ -4,7 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("Hello, World.");
+        System.out.println("æ»≥Á, ºº∞Ë.");
+        System.out.println(∫∏¿Ã∑Á, ∫∏∞‚æ≤.");"
+        
+        
+        
 	}
 
 }
